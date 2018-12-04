081ab69bff9ccf87f7324ae5662d887f2949a751
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-04 14:13:44 +0500

    Update Program.cs

--- Ch19-P9-ThreadPoolApp/Program.cs
+++ Ch19-P9-ThreadPoolApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch19_P9_ThreadPoolApp
@@ -10,7 +11,18 @@ namespace Ch19_P9_ThreadPoolApp
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("***** Fun with the CLR Thread Pool *****\n");
+            Console.WriteLine("Main thread started. ThreadID = {0}",
+            Thread.CurrentThread.ManagedThreadId);
+            Printer p = new Printer();
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+            // Queue the method ten times.
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+            Console.WriteLine("All tasks queued");
+            Console.ReadLine();
         }
     }
 }
